29a5c143a281ef0ebcaa25ce2628ed6dee8633df
rishabh <rishabh@apples-MacBook-Pro.local>
2016-12-10 21:26:44 +0000

    Added wait functionality when TwitterException is caught. Implemented sort functionality for mapLikes.

--- src/Stalker.java
+++ src/Stalker.java
@@ -1,14 +1,9 @@
 import twitter4j.*;
 
-import java.util.HashMap;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
-/**
- * Created by rishabh on 09/12/16.
- */
 public class Stalker {
-  public static void main(String[] args) throws TwitterException {
+  public static void main(String[] args) throws TwitterException, InterruptedException {
     TwitterFactory tf = new TwitterFactory();
     Twitter twitter = tf.getInstance();
 
@@ -28,22 +23,34 @@ public class Stalker {
 
     User userStalk = searchResults.get(choice);
 
-//    FriendsFollowersResources fnfResourses = twitter.friendsFollowers();
     HashMap<Long, Integer> mapLikes = new HashMap<Long, Integer>();
     HashMap<Long, String> mapName = new HashMap<Long, String>();
     int flag = 1;
     int totalLikes = 0;
-    while (flag != 0) {
+    while (true) {
       Paging paging = new Paging();
       paging.setCount(userStalk.getFavouritesCount());
-      ResponseList<Status> favourites = twitter.getFavorites(userStalk.getId(),
-          new Paging(flag++, 1000));
+      ResponseList<Status> favourites = null;
+      try {
+        favourites = twitter.getFavorites(userStalk.getId(), new Paging(flag, 1000));
+      } catch (TwitterException te) {
+        System.out.println("twitter exception caught");
+        Date date = new Date();
+        int timeRemaining = te.getRateLimitStatus().getSecondsUntilReset();
+        long timeStop = date.getTime() + timeRemaining * 1000;
+        System.out.println("started waiting... " + date);
+        while (new Date().getTime() < timeStop) {
+        }
+        System.out.println("...stopped waiting " + new Date());
+        continue;
+      }
+      flag++;
+
       totalLikes += favourites.size();
-      if(favourites.size() == 0) {
-        flag = 0;
+      if (favourites.size() == 0) {
+        break;
       }
       for (Status status : favourites) {
-
         if (mapLikes.get(status.getUser().getId()) == null) {
           mapLikes.put(status.getUser().getId(), 1);
           mapName.put(status.getUser().getId(), status.getUser().getName());
@@ -54,11 +61,43 @@ public class Stalker {
       }
     }
     System.out.println("total likes: " + totalLikes);
+    mapLikes = sortHashMapByValues(mapLikes);
     Set<Long> userIds = mapLikes.keySet();
     for (long userId : userIds) {
       int likes = mapLikes.get(userId);
       String name = mapName.get(userId);
-      System.out.println(name +  '\t' + '\t' + '\t' + likes);
+      System.out.println(name + " " + likes);
     }
   }
+
+  public static HashMap<Long, Integer> sortHashMapByValues(
+      HashMap<Long, Integer> passedMap) {
+    List<Long> mapKeys = new ArrayList<Long>(passedMap.keySet());
+    List<Integer> mapValues = new ArrayList<Integer>(passedMap.values());
+    Collections.sort(mapValues);
+    Collections.sort(mapKeys);
+
+    HashMap<Long, Integer> sortedMap =
+        new LinkedHashMap<Long, Integer>();
+
+    Iterator<Integer> valueIt = mapValues.iterator();
+    while (valueIt.hasNext()) {
+      int val = valueIt.next();
+      Iterator<Long> keyIt = mapKeys.iterator();
+
+      while (keyIt.hasNext()) {
+        Long key = keyIt.next();
+        int comp1 = passedMap.get(key);
+        int comp2 = val;
+
+        if (comp1 == comp2) {
+          keyIt.remove();
+          sortedMap.put(key, val);
+          break;
+        }
+      }
+    }
+    return sortedMap;
+  }
+
 }
